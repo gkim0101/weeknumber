31c8860ff154c14acfdbb1013c88682c440269d2
bob <gkim0101@gmail.com>
2022-04-02 13:30:19 -0700

    first

---
+++ weeknum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+
+int week_num(const std::tm& tm){
+	constexpr int DAYS_PER_WEEK=7;
+
+	const int wday = tm.tm_wday;
+	const int delta = wday ? wday-1 : DAYS_PER_WEEK-1;
+	return (tm.tm_yday + DAYS_PER_WEEK - delta)/DAYS_PER_WEEK;
+}
+
+int main(){
+	std::time_t t=std::time(0);
+	std::tm* now = std::localtime(&t);
+	std::cout<<(now->tm_year+1900)<<'-'
+		<< (now->tm_mon +1) <<'-'
+		<< now->tm_mday
+		<<std::endl;
+	
+	std::cout<<week_num(now)<<std::endl;
+	
+
+	return 0;
+}
