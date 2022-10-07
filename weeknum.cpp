0a4fa43f98c31c7ebd54403c2c73d76adfca576b
Bob Kim <gilnam.kim@motional.com>
2022-10-07 13:24:58 -0700

    clang-format-7

--- weeknum.cpp
+++ weeknum.cpp
@@ -1,24 +1,21 @@
-#include <iostream>
 #include <ctime>
+#include <iostream>
 
-int week_num(const std::tm& tm){
-	constexpr int DAYS_PER_WEEK=7;
+int week_num(const std::tm &tm) {
+  constexpr int DAYS_PER_WEEK = 7;
 
-	const int wday = tm.tm_wday;
-	const int delta = wday ? wday-1 : DAYS_PER_WEEK-1;
-	return (tm.tm_yday + DAYS_PER_WEEK - delta)/DAYS_PER_WEEK;
+  const int wday = tm.tm_wday;
+  const int delta = wday ? wday - 1 : DAYS_PER_WEEK - 1;
+  return (tm.tm_yday + DAYS_PER_WEEK - delta) / DAYS_PER_WEEK;
 }
 
-int main(){
-	std::time_t t=std::time(0);
-	std::tm* now = std::localtime(&t);
-	std::cout<<(now->tm_year+1900)<<'-'
-		<< (now->tm_mon +1) <<'-'
-		<< now->tm_mday
-		<<std::endl;
-	
-	std::cout<<week_num(*now)<<std::endl;
-	
+int main() {
+  std::time_t t = std::time(0);
+  std::tm *now = std::localtime(&t);
+  std::cout << (now->tm_year + 1900) << '-' << (now->tm_mon + 1) << '-'
+            << now->tm_mday << std::endl;
 
-	return 0;
+  std::cout << week_num(*now) << std::endl;
+
+  return 0;
 }
